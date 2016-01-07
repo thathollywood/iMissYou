8b36b149d9944565c6df203d378ae2f0ff123eb4
Martin Hollywood <martin@thathollywood.com>
2016-01-07 12:51:27 +0000

    1st commit
    Personal details removed. Added Fritzing schematic

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # iMissYou
 Capacitance-triggered PUSH notification for Arduino YUN
+
+My entry for the [Arduino Uno WiFi XMAS Contest 2015](http://www.arduino.org/arduino-xmas-2015)
+
+**What is it?**
+
+A Peer2Peer Photoframe that when touched, communicates with a Service that sends a PUSH notification to a phone: ‘I’m thinking of you’.
+This MVP was developed with the Arduino Yun, a capacitive layer (behind the photo), and the Pushover notification service.
+
+
+**Result?**
+
+Brings together loved ones separated by distance in a tactile way.
+
+I touch the faces on my family’s photo - Instantly, anywhere in the world, they know I’m thinking of them.
+
+
+**HARDWARE**
+
+* [Arduino YUN](https://www.arduino.cc/en/Main/ArduinoBoardYun)
+* 2 LEDs for Status: Online/Ready & Sending (I soldered these to a test rig).
+* Conductive material to create capactive 'aerial'. I used cardboard and Bare Conductive's [Electric Paint](http://www.bareconductive.com/shop/electric-paint-50ml/)
+* Assorted wires
+* [Pushover account](https://pushover.net) for notifications. 
+
+![image](http://www.thathollywood.com/wp-content/uploads/2015/06/IMG_0146-1024x768.jpg)
+![image](http://www.thathollywood.com/wp-content/uploads/2015/06/IMG_2899.jpg)
+
+More information on the project can be found on my [blog](http://www.thathollywood.com/imissyou-a-new-p2p-product-for-the-internet-of-things/)
---
+++ iMissYou.ino
@@ -0,0 +1,173 @@
+// PUSHOVER ---------------->>
+/*
+  Expansion of Yún HTTP Client example http://arduino.cc/en/Tutorial/HttpClient
+  Pushover service > https://pushover.net
+  Emoji Hex Codes > http://apps.timwhitlock.info/emoji/tables/unicode
+ */
+
+#include <Bridge.h>
+#include <YunClient.h>
+
+// Pushover settings - create an account to get Token & Key
+char pushoversite[] = "api.pushover.net";
+char apitoken[] = "your30characterapitokenhere123";
+char userkey [] = "your30characteruserkeygoeshere";
+
+int length;
+
+// Initialize the client library
+YunClient client;
+
+// CAPACITANCE ---------------->>
+#include <CapacitiveSensor.h>
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+int val;
+boolean capState = false;
+boolean caliState = false;
+
+long start;
+long total1;
+
+// variable to hold sensor value
+int calVal;
+// variable to calibrate low value
+int calL = 10;
+// variable to calibrate high value
+int calH = 30;
+
+// LED pin - use to show status of events
+const int ledReady = 8;
+const int ledSend = 9;
+
+// Messages to send & emoticons -------------------------->>
+
+char *think[]={"Counting the hours until he can be with you \xE2\x8F\xB3", "He's thinking of you \xF0\x9F\x98\x8D","There's nowhere else on this world that he'd rather be than beside you right now \xE2\x9B\xB5\xF0\x9F\x8C\xB4", "He touched your face!", "F'k em all. Let's run off and live in a yurt!"};
+long thinking;
+
+
+void setup() {
+  // Bridge takes about two seconds to start up
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  Serial.begin(9600);
+  
+  Serial.println("hello");
+  
+  pinMode(ledReady, OUTPUT);
+  pinMode(ledSend, OUTPUT);
+
+  digitalWrite(ledReady, HIGH);
+  digitalWrite(ledSend, HIGH);
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);    
+  
+  // calibrate background value - do not touch sensor!
+  Serial.println("CALIBRATE....");
+  long currentMillis = millis();
+  long interval = 5000;  
+  while (millis() < 5000) {
+    Serial.println(millis());
+   total1 =  cs_4_2.capacitiveSensor(300);
+      // record the maximum sensor value
+      calVal = (int)total1;
+      if (calVal > calH) {
+        calH = calVal;
+      }
+      // record the minimum sensor value
+      if (calVal < calL) {
+        calL = calVal;
+      }
+    Serial.print("cal: ");
+    Serial.print(calL);        // check on performance in milliseconds
+    Serial.print("\t");        // tab character for debug window spacing
+    Serial.println(calH);      // print sensor output 1
+    }
+
+ // while (!Serial); // wait for a serial connection
+   Serial.println("CALIBRATED");
+   digitalWrite(ledSend, LOW);
+   digitalWrite(ledReady, HIGH);
+}
+
+
+
+void loop() {
+   if (!capState){
+     cap();
+     delay(100);                             // arbitrary delay to limit data to serial port 
+   } else {
+     thinking = random(sizeof(think)/sizeof(char*));
+     Serial.println(think[thinking]);
+     pushover(think[thinking],0);  
+     capState = false; 
+     
+     const int oneMinute = (1000 * 60);
+     const int coolit = oneMinute * 5;
+     delay(coolit);
+   }
+   
+}
+
+
+void cap(){
+    start = millis();
+   total1 =  cs_4_2.capacitiveSensor(300);
+
+    val = (int)total1;
+    //Serial.println(val);
+
+   if(val > (calH + 50)){
+    Serial.println("ON!");
+    digitalWrite(ledSend, HIGH);
+    digitalWrite(ledReady, LOW);
+
+    capState = true; // PUSH!
+   
+   } else {
+    digitalWrite(ledSend, LOW);
+    digitalWrite(ledReady, HIGH);
+   }
+
+}
+
+
+
+byte pushover(char *pushovermessage, int priority)
+{
+  String message = pushovermessage;
+
+  length = 113 + message.length();
+// more info on parameters https://pushover.net/api 
+  if(client.connect(pushoversite,80))
+  {
+    client.println("POST /1/messages.json HTTP/1.1");
+    client.println("Host: api.pushover.net");
+    client.println("Connection: close\r\nContent-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.print(length);
+    client.println("\r\n");;
+    client.print("token=");
+    client.print(apitoken);
+    client.print("&user=");
+    client.print(userkey);
+    client.print("&device=work"); // << TEST DEVICE
+    client.print("&title=iMissYou");
+    client.print("&sound=bike");
+    client.print("&message=");
+    client.print(message);
+    client.print("&priority=");
+    client.print(priority);
+    client.print("&retry=60");
+    client.print("&expire=3600");
+    
+    while(client.connected())  
+    {
+      while(client.available())
+      {
+        char ch = client.read();
+        Serial.write(ch);
+      }
+    }
+    client.stop();
+  }  
+}
